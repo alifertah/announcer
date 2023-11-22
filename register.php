36767aeaca9ab67818c00ec97742709d66016c2f
alifertah <alfertah@student.1337.ma>
2023-11-22 14:38:02 +0100

    feat : register page that insert the informations to db

--- register.php
+++ register.php
@@ -1,21 +1,21 @@
 <?php
 include("conect.php");
-
-if(isset($_POST["login"])){
-    $uname = $_POST["user"];
-    $pass = $_POST["password"];
-    $query = "SELECT * FROM `user` where username = '$uname' and password = '$pass';"; 
-
-    $result = $con->query($query);
     
-    $error = "";
-    if($result->num_rows == 0){
-      // $row = $result->fetch_assoc();
-      $error = "invalide password or username!";
-    }
-    $uname = "";
-    $pass = "";
-    $result = "";
+if(isset($_POST["register"])){
+  $uname = $_POST["user"];
+  $pass = $_POST["password"];
+  $email = $_POST["email"];
+  
+  $query = "INSERT INTO `user` (
+    `username`, `password`, `email`
+  ) VALUES 
+  ('$uname', '$pass', '$email')";
+
+  if($con->query($query)){
+    echo("YES");
+  } else {
+    echo "ERROR : " . $query . $con->error;
+  }
 }
 ?>
 <!DOCTYPE html>
@@ -24,17 +24,26 @@ if(isset($_POST["login"])){
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://cdn.tailwindcss.com"></script>
-    <title>index</title>
+    <title>register</title>
 </head>
 <body class="flex justify-center">
+
 <div class="w-full max-w-xs">
-  <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" action="index.php" method="post" name="login-form">
+  <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" action="register.php" method="post" name="register-form">
     <div class="mb-4">
       <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
         Username
       </label>
       <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="user" type="text" name="user" placeholder="Username">
     </div>
+
+    <div class="mb-4">
+      <label class="block text-gray-700 text-sm font-bold mb-2" for="email">
+        Email
+      </label>
+      <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="email" type="email" name="email" placeholder="email">
+    </div>
+
     <div class="mb-6">
       <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
         Password
@@ -43,7 +52,7 @@ if(isset($_POST["login"])){
       <?php if(isset($error)){echo "<p class='text-red-500 text-xs italic'> $error </p>";}  ?>
     </div>
     <div class="flex items-center justify-between">
-      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit" name="login" value="login">
+      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit" name="register" value="register">
         Sign In
       </button>
       <a class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="#">
@@ -52,5 +61,6 @@ if(isset($_POST["login"])){
     </div>
   </form>
 </div>
+
 </body>
 </html>
